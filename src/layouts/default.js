4df37f34626f60527256da4e141e3d5993bc227f
Trevor Adams <trevorwadams@outlook.com>
2019-06-27 12:05:10 -0600

    task: remove styling

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from '../components/header'
-import './default.css'
 
 const DefaultLayout = ({ children }) => (
   <StaticQuery
@@ -19,14 +18,7 @@ const DefaultLayout = ({ children }) => (
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
           {children}
         </div>
       </>
